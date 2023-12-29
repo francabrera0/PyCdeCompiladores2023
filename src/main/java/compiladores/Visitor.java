403c022c9ef38e486f4a3d175dc74d5774816ee5
francabrera0 <francabrera0@gmail.com>
2023-12-29 19:37:41 -0300

    LogicalArithmeticExpression finished

--- src/main/java/compiladores/Visitor.java
+++ src/main/java/compiladores/Visitor.java
@@ -70,6 +70,8 @@ public class Visitor extends compiladoresBaseVisitor<String> {
 
     /**
      * Visita a los hijos (logicalExpression)
+     * 
+     * logicalArithmeticExpression : logicalExpression;
      */
     @Override
     public String visitLogicalArithmeticExpression(LogicalArithmeticExpressionContext ctx) {
@@ -80,40 +82,43 @@ public class Visitor extends compiladoresBaseVisitor<String> {
     /**
      * Visita a los hijos ()
      * Tengo que trabajar con esta regla ahora para agregar los and or o cmp    
+     * 
      * logicalExpression : logicalExpression AND logicalExpression
      *                   | logicalExpression OR logicalExpression
      *                   | arithmeticExpression CMP arithmeticExpression
      *                   | arithmeticExpression
-     *                   ;
-     *      CODIGO REPETIDO EN CMP  
+     *                   ; 
      */
     @Override
     public String visitLogicalExpression(LogicalExpressionContext ctx) {
                 
         if(ctx.getChild(1) == null) { //En este caso es unicamente una opal.
-
-            visitArithmeticExpression(ctx.arithmeticExpression(0));
-            
-            if(preOrPost == 1) //pre
-                treeAddressCode += incDecInstruction;
-        
-            String newVariable = variableGenerator.getNewVariable();
-
-            treeAddressCode += "\n" + newVariable + " = " + operands.pop(); 
-
-            if(preOrPost == 2) //post
-                treeAddressCode+= incDecInstruction;
-
-            preOrPost = 0;
-            incDecInstruction = "";
-
-            operands.push(newVariable);
-
+            visitArithmeticExpression(ctx.arithmeticExpression(0)); 
         }
-
         else {
             if(ctx.getChild(1).getText().equals("&&") || ctx.getChild(1).getText().equals("||")) { //AND/OR
-                System.out.println("AND/OR");
+                /**
+                 * Lo que hace es: - Busca el primer operando
+                 *                 - Este seguramente sea o una cmp o una arithm
+                 *                 - Luego busca el segundo ( el segundo puede ser otra && u ||) 
+                 *                 - En caso que sea otra && u || cuando hace el visit logical "vuelve a empezar" dejando en la pila de operandos el primero
+                 *                 - Cuando ya no hay && u || entonces empieza a armar el tac con todos los op que fue apilando
+                 *                 - Es recursivo
+                 */
+
+                visitLogicalExpression(ctx.logicalExpression(0));
+                visitLogicalExpression(ctx.logicalExpression(1));
+
+                String newVariable = variableGenerator.getNewVariable();
+                String firstOperand = operands.pop();
+                String secondOperand = operands.pop();
+                String operator = ctx.getChild(1).getText();
+
+                treeAddressCode += "\n" + newVariable + " = " + firstOperand + operator + secondOperand;
+                operands.push(newVariable);
+
+                return treeAddressCode;
+
             }
 
             else { //CMP solo permite comparar 2 aritmeticas
@@ -138,10 +143,13 @@ public class Visitor extends compiladoresBaseVisitor<String> {
     /**
      * Visita a los hijos de la expAritmetica, esots son aritmeticTerm y at.
      * Aca lo que hago es poner en una nueva var el resultado de la expresion.
+     * 
+     * arithmeticExpression : arithmeticTerm at;
      */
     @Override
     public String visitArithmeticExpression(ArithmeticExpressionContext ctx) {
         visitChildren(ctx);
+
         String newVariable = variableGenerator.getNewVariable();
         
         if(preOrPost == 1) //pre
@@ -162,6 +170,8 @@ public class Visitor extends compiladoresBaseVisitor<String> {
     
     /**
      * Visita a los hijos de arithmeticTerm, estos son factor, af
+     * 
+     * arithmeticTerm : factor af;
      */
     @Override
     public String visitArithmeticTerm(ArithmeticTermContext ctx) {
@@ -173,6 +183,14 @@ public class Visitor extends compiladoresBaseVisitor<String> {
      * Visitar un facto quiere decir que estamos en la hoja de esta rama. En caso de que el factor
      * sea directamente el operando (ID, NUMBER, CHAR), se coloca en la lista de operandos el valor.
      * En caso de ser un incDec, functionCall u otra expresion AL, se llama al visitor correspondiente.
+     * 
+     * factor : NUMBER
+     *        | CHARACTER
+     *        | ID
+     *        | PARENTHESES_O logicalArithmeticExpression PARENTHESES_C
+     *        | incDec
+     *        | functionCall
+     *        ;
      */
     @Override
     public String visitFactor(FactorContext ctx) {
@@ -204,6 +222,10 @@ public class Visitor extends compiladoresBaseVisitor<String> {
      * en función de si es inc o dec arma una instrucción id = id +/- 1; y la almacena en incDecInstruction
      * Estas dos variables se usan luego para insertar el inc o dec en el TAC.
      * Por último, coloca en operands el ID de la variable incrementada.
+     * 
+     * incDec : INCDECOPERATORS ID
+     *        | ID INCDECOPERATORS
+     *        ;
      */
     @Override
     public String visitIncDec(IncDecContext ctx) {
@@ -239,6 +261,11 @@ public class Visitor extends compiladoresBaseVisitor<String> {
      * En caso de que el contexto no tenga mas factores se retorna. Si los tiene se hace una visita
      *  y en la lista de operands retornará el nuevo operando producto de la visita. Luego se arma el TAC.
      * Por último veerifica si hay mas anidaciones.
+     * 
+     * af : MUL factor af
+     *    | DIV factor af
+     *    |
+     *    ;
      */
     @Override
     public String visitAf(AfContext ctx) {
@@ -294,6 +321,11 @@ public class Visitor extends compiladoresBaseVisitor<String> {
      * lista y realiza una visita al otro termino. EN operands volverá el operando.
      * Arma el TAC.
      * Verifica si hay anidaciones. 
+     * 
+     * at : ADD arithmeticTerm at
+     *    | SUB arithmeticTerm at
+     *    |
+     *    ;
      */
     @Override
     public String visitAt(AtContext ctx) {
