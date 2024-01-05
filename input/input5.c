b96ae7f69ced50b8b8bcb1e8bf45c9dc41c85b00
francabrera0 <francabrera0@gmail.com>
2024-01-05 10:46:10 -0300

    Add new input file

---
+++ input/input5.c
@@ -0,0 +1,43 @@
+int calcular(int x, int y);
+
+void a();
+
+int calcular2(int x) {
+    return calcular(x,2)*3;
+}
+
+int main() {
+    int z,j = 33; 
+    int i =++j + 00004;
+    int x = calcular(00000000+ ++i, 3+j++)*i++ + 5*(904045/3-1) == 4 || 9*(3+2)+calcular(i*3,3*j);
+
+    i = j/j++;
+    
+    a();
+    
+    i = calcular2(i);
+
+    x = --j + ++i;
+    i = 2+x;
+    j = 3;
+    if(i < j)
+        x = ++i * ++j;
+    else if(i>j){
+        for(int i = 3; i < 5; ++i)
+            z = 3;
+    }
+    else 
+        x = 4 + j;
+
+    return x*z;
+}
+
+void a(){
+    int b = 3;
+    b++;
+
+}
+
+int calcular(int x, int y) {
+    return x + y;
+}
